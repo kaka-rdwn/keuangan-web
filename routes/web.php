2d7e6d6ab9d284843ed038e34bd37ad3846bad26
kaka-rdwn <halo.kakaridwan@gmail.com>
2026-08-12 11:23:12 +0700

    feat: implement authentication layouts and feature tests while configuring web routes

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::redirect('/', '/dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', DashboardController::class)->name('dashboard');
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-test('returns a successful response', function () {
-    $response = $this->get(route('home'));
+test('root path redirects to dashboard', function () {
+    $response = $this->get('/');
 
-    $response->assertOk();
+    $response->assertRedirect('/dashboard');
 });
